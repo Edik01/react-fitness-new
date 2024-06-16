9c235dbe64fcce63c2539108130a35e4356456f4
Edik Huloian <guloyan.edik@gmail.com>
2024-06-16 09:54:59 -0700

    sumarry

--- app/HomeScreen.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-
-const HomeScreen = () => {
-  return (<View style={styles.container}></View>
-    <Text>Добро подаловать в Fitness App!</Text>
-  );
-};
